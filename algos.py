3076921776ae86b06762bac70da5eb8b93377bee
Adam Joines <amountcastlej@gmail.com>
2021-09-16 22:48:44 -0400

    "python algos added"

--- algos.py
+++ algos.py
@@ -130,3 +130,14 @@ def array_front9(nums):
 array_front9([1, 2, 9, 3, 4])
 array_front9([1, 2, 3, 4, 9])
 array_front9([1, 2, 3, 4, 5])
+
+#Array123
+#Given an array of ints, return True if the sequence of numbers 1,2,3 appears in the array somewhere
+def array123(nums):
+	for i in range(len(nums)-2):
+    	if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
+		return True
+	return False
+array123([1, 1, 2, 3, 1]) 
+array123([1, 1, 2, 4, 1])
+array123([1, 1, 2, 1, 2, 3])
